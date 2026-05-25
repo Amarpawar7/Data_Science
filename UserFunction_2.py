227e12a7f86cfe593862cd3978e433a4dce2db66
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-05-25 15:21:30 +0530

    Add files via upload

---
+++ UserFunction_2.py
@@ -0,0 +1,13 @@
+def Multiplication (Value1, Value2):
+    Ans=0           #Local Variable: variable declared inside the function
+    Ans = Value1 * Value2
+    return Ans
+
+print("Demo application : ") 
+   
+no1 = 10
+no2 = 11
+Result = 0
+
+Result = Multiplication(no1,no2)
+print("multiplication is : ",Result)
