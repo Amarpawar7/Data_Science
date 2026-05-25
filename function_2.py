0e1aca11e45fbca8e500095b93a2976c9a96b5da
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-05-25 15:20:47 +0530

    Add files via upload

---
+++ function_2.py
@@ -0,0 +1,9 @@
+print("Enter first number : ")
+no1 = input()                   # by default input assigned is of string datatype so error occurs
+print("Enter second number : ")
+no2 = input()
+
+Ans = no1 * no2
+
+print("Multiplication is : ",Ans)
+
