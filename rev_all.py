f2fcb91d0378270362942c9abaaf4f6ad492938c
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-05-25 15:06:02 +0530

    Add files via upload

---
+++ rev_all.py
@@ -0,0 +1,12 @@
+#Set:
+arr= {10,20,30,40,10}   #no duplicates so no indexing
+print(arr)
+print(type(arr))
+print("-"*40)
+
+#Dictionary: 
+book = {"Name" : "Let us C" , "Price": 350.50 , "Author" : "Y Kanetkar" }
+print(book)
+print(type(book))
+print(book["Price"])   #access particular key value
+print("-"*40)
