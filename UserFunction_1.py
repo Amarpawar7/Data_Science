53c1b9311855316b4ef170f0aaa479ccb2f44e22
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-05-25 15:21:19 +0530

    Add files via upload

---
+++ UserFunction_1.py
@@ -0,0 +1,7 @@
+def Multiplication (Value1, Value2):
+    Ans=0           #Local Variable: variable declared inside the function
+    Ans = Value1 * Value2
+    return Ans
+
+print("Demo application : ") 
+   
