40c36b1f0cda288459138a461be39b10d340a2c4
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-05-25 15:21:11 +0530

    Add files via upload

---
+++ function_4.py
@@ -0,0 +1,21 @@
+print("Enter first number : ")
+no1 = int(input())                        # by default input assigned is of string datatype
+print("Enter second number : ")
+no2 = int(input())
+
+Ans = no1 * no2     #this line is called ad=s buisness logic(i define what was actually done)
+
+print("Multiplication is : ",Ans)
+
+
+
+###############################################
+print("Enter first number : ")
+no1 = int(input())                        # by default input assigned is of string datatype
+print("Enter second number : ")
+no2 = int(input())
+
+Ans = no1 * no2
+
+print("Multiplication is : ",Ans)
+
