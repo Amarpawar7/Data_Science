07d7bdb473e85e5a7b6820f796e5c3eee0fa8de6
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-05-27 01:31:41 +0530

    Add files via upload

---
+++ FunctionDemo_6.py
@@ -0,0 +1,13 @@
+# Accept : Multiple Parameters
+# Return : Nothing
+
+def Demo(Value1,Value2):
+    print("Inside Demo : ",Value1, Value2)
+    
+
+def main():
+    Demo("Python", 21)
+
+if __name__ == "__main__":
+    main()
+    
