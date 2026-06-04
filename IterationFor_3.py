81c69124cfc51a24dc585f3168b8be8b921b3de8
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-06-04 23:15:36 +0530

    Add files via upload

---
+++ IterationFor_3.py
@@ -0,0 +1,14 @@
+#Sequence
+
+print("Hello World")
+print("Hello World")
+print("Hello World")
+print("Hello World")
+
+print("-"*20)
+
+#Iteration 
+
+for i in range(4):
+    print("Hello World")
+
