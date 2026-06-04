7fe2faa7dc5cbbd499f46e3d013d8a30ba7caa4e
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-06-04 23:14:44 +0530

    Add files via upload

---
+++ Lambda_2.py
@@ -0,0 +1,23 @@
+#Functional Approach
+
+# def CheckEven(num):
+#     return(num%2 == 0)
+
+CheckEven = lambda num : (num%2 == 0)
+
+def main():
+    Value = 0           #by putting default value we assigned default values of the datatype as int
+    Ret = False
+
+    print("Enter number : ")
+    Value = int(input())
+
+
+    Ret = CheckEven(Value)
+    if(Ret == True):
+        print("It is Even")
+    else: 
+        print("It is Odd")
+
+if __name__ == "__main__":
+    main()
