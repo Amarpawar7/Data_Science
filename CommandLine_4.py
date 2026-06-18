6e2aa215bdb957bf9eee9b0df6f9eeb70e23697d
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-06-19 00:17:11 +0530

    Add files via upload

---
+++ CommandLine_4.py
@@ -0,0 +1,27 @@
+# python  CommandLine_4.py  10 21
+
+# import sys
+
+# def main():
+    
+#     print("Addition is : ")
+    
+#     print(int(sys.argv[1])+int(sys.argv[2]))
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+import sys
+
+def main():
+    No1 = int(sys.argv[1])
+    No2 = int(sys.argv[2])
+
+    print(No1 + No2)
+    
+    
+if __name__ == "__main__":
+    main()
