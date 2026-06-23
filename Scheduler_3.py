30df4ada321ba9ee2a77f969c810a1d87f2deccc
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-06-23 23:38:24 +0530

    Add files via upload

---
+++ Scheduler_3.py
@@ -0,0 +1,20 @@
+import time
+import datetime
+import schedule
+
+def fun():
+    print("Inside fun at : ",datetime.datetime.now())
+
+def main():
+    print("Inside Automation script at : ",datetime.datetime.now())
+
+    schedule.every(20).seconds.de(fun)
+    
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ =="__main__":
+    main()
+
