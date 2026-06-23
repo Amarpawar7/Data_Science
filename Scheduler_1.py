cf5111e6783f648622c635bbc6edd7ea7436178f
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-06-23 23:38:01 +0530

    Add files via upload

---
+++ Scheduler_1.py
@@ -0,0 +1,10 @@
+import time
+import datetime
+
+def main():
+    print(time.time())                  # It's the time from 1 jan 1970  - called as linux ....
+    print(time.ctime())                 # Current time
+    print(datetime.datetime.now())      #
+
+if __name__ =="__main__":
+    main()
