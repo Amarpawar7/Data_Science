396859d249fc4632171631528ca6fae140845e6e
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-07-12 01:47:49 +0530

    Add files via upload

---
+++ ChecksumCalculateDirectoryStore.py
@@ -0,0 +1,88 @@
+import hashlib
+import os
+
+def CalculateCheckSum(FileName):
+    fobj = open(FileName, "rb")
+
+    hobj = hashlib.md5()
+
+    Buffer = fobj.read(1000)
+    
+    while(len(Buffer) > 0):
+        hobj.update(Buffer)
+        Buffer = fobj.read(1000) 
+
+    fobj.close()
+
+    return hobj.hexdigest()
+
+
+def FindDuplicate(DirectoryName = "Demo"):
+    Ret = False
+    Ret = os.path.exists(DirectoryName)
+
+    if (Ret == False):
+        print("there is no such directory ")
+        return
+    
+    Ret = os.path.isdir(DirectoryName)
+    
+    if (Ret == False):
+        print("It is not a directory")
+        return
+
+    Duplicate = {}
+
+    for FolderName, SubFolderName , FileName in os.walk(DirectoryName):
+        for fname in FileName:
+            fname = os.path.join(FolderName, fname)
+            Checksum = CalculateCheckSum(fname)
+
+            if Checksum in Duplicate :
+                Duplicate[Checksum].append(fname)                  # Checksum acts as a key for our dictionary-
+            else:
+                Duplicate[Checksum] = [fname]
+
+    return Duplicate
+
+
+def DisplayResult(MyDict):
+    Result = list(filter(lambda x : len(x) > 1, MyDict.values()))
+
+    count = 0 
+    for value in Result:
+        for subvalue in value:
+            count = count + 1
+            print(subvalue)
+        print("value of count is : ",count)
+        count = 0
+
+
+def DeleteDuplicate(Path ="Demo"):
+    MyDict = FindDuplicate(Path)
+
+    Result = list(filter(lambda x : len(x) > 1, MyDict.values()))
+
+    count = 0 
+    cnt = 0
+
+    for value in Result:
+        for subvalue in value:
+            count = count + 1
+            if(count > 1):
+                print("Deleted file : ",subvalue)
+                os.remove(subvalue)
+                cnt = cnt+1
+        count = 0
+    
+    print("Total Deleted files : ",cnt)
+
+
+def main():
+
+    DeleteDuplicate()
+    
+
+if __name__ == "__main__":
+    main()
+
