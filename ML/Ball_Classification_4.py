cf144fe5bab1898f55f527bf5aaa8fc13bd08834
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-07-24 23:33:41 +0530

    Add files via upload

---
+++ ML/Ball_Classification_4.py
@@ -0,0 +1,34 @@
+from sklearn import tree
+
+# following is one hot encoding
+# Rough = 1
+# Smooth = 0
+
+# Tennis = 1
+# Cricket = 2
+
+def main():
+    print("Ball Classification Case Study")
+
+    # Data Loading / Gathering
+
+    # Independent Variables
+    X = [[35,1],[47,1],[90,0],[48,1],[90,0],[35,1],[92,0],[35,1],[35,1],[35,1],[96,0],[43,1],[110,0],[35,1],[95,0]] 
+
+    # Dependent Variables
+    Y = [1,1,2,1,2,1,2,1,1,1,2,1,2,1,2]
+
+    modelobj = tree.DecisionTreeClassifier()
+    
+    trainedmodel = modelobj.fit(X,Y)
+
+    Result = trainedmodel.predict([[37,1],[94,0]])     # output : [1 2]
+
+    print("Model Predictes the object as : ",Result)
+
+
+
+if __name__ == "__main__":
+    main()
+ 
+# Dataset Size : 15
