6bd33710b2c6168e72fc74642725898a64d00eae
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-08-12 23:31:27 +0530

    Add files via upload

---
+++ ML/DecisionTree_1.py
@@ -0,0 +1,33 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split         # method names are yellow
+from sklearn.tree import DecisionTreeClassifier                 # class naem are green
+from sklearn.metrics import accuracy_score
+
+
+
+# we can use it when dataset is large,coz it takes time while training but gives fastest result
+
+
+
+def main():
+
+    iris = load_iris()
+
+    X = iris.data
+    Y = iris.target
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2)
+
+    model = DecisionTreeClassifier()
+
+    model.fit(X_train, Y_train)
+
+    Y_pred = model.predict(X_test)
+
+    accuracy = accuracy_score(Y_test,Y_pred)
+
+    print("Accuracy is : ",accuracy*100)
+
+
+if __name__ == "__main__":
+    main()
