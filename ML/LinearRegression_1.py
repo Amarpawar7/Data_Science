0d645815bad838c6ec48e960d5a5e47217f1de77
Amar Pawar <156612323+Amarpawar7@users.noreply.github.com>
2026-08-14 16:56:58 +0530

    Add files via upload

---
+++ ML/LinearRegression_1.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+def Predictor():
+    # Load the data
+    X = [1,2,3,4,5]
+    Y = [3,4,2,4,5]
+
+    print("Values of Independent variables : X - ",X)
+    print("Values of Dependent variables : Y - ",Y)
+
+    mean_x = np.mean(X)
+    mean_y = np.mean(Y)
+    
+    print("X_Mean is : ",mean_x)
+    print("Y_Mean is : ",mean_y)
+
+    
+
+
+def main(): 
+    Predictor()    
+
+
+
+if __name__ == "__main__":
+    main()
